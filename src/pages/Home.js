ecea663595cbd39b52fb4640d53bff3196dd1c72
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-11-07 01:02:30 +0000

    created component for the home page

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BlogList from '../components/BlogList';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Welcome to ByteWise Blog</h1>
+        <BlogList />
+      </main>
+    </>
+  );
+};
+
+export default Home;
